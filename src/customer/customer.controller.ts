011e1c838000db7b06f6d51cafa31d31e01dd1fe
Hyeongwook Kim <jahanda@naver.com>
2022-04-06 00:19:32 +0900

    modify

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -5,25 +5,16 @@ import { NewCustomerInfo } from './customer.interface';
 import { CreateCustomerInfoDto } from './dtos/create-customer-info.dto';
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { CustomerService } from './customer.service';
-<<<<<<< HEAD
 
-=======
->>>>>>> cc29c55ffa7983f84c32aaa73b7f7290dab7d18f
 @Controller('customer')
 export class CustomerController {
   constructor(private readonly customerService: CustomerService) {}
 
   @Post()
   async createAccount(
-<<<<<<< HEAD
-    @Body() createCustomerInfoDto: CreateCustomerInfoDto,
-  ): Promise<boolean> {
-    return await this.customerService.createAccount(createCustomerInfoDto);
-=======
     @Body() creatCustomerInfoDto: CreateCustomerInfoDto,
   ): Promise<NewCustomerInfo> {
     return await this.customerService.createAccount(creatCustomerInfoDto);
->>>>>>> cc29c55ffa7983f84c32aaa73b7f7290dab7d18f
   }
 
   @Post('/email-verify')
